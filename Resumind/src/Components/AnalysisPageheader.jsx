af8515db8fc5e5079022d870d0d4862c468f653d
ty2arjit <ty2arjit@gmail.com>
2025-08-03 20:14:02 +0530

    name fixed

--- Resumind/src/Components/AnalysisPageheader.jsx
+++ Resumind/src/Components/AnalysisPageheader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./AnalysisPageheader.module.css";
+import styles from "./AnalysisPageHeader.module.css";
 
 const AnalysisPageheader = () => {
   return (
