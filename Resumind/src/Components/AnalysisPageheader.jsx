1c2b9a53bcd338e6ec45c56a74d9d421cfaae911
ty2arjit <ty2arjit@gmail.com>
2025-08-03 20:34:30 +0530

    analysis pageheader module fixed

--- Resumind/src/Components/AnalysisPageheader.jsx
+++ Resumind/src/Components/AnalysisPageheader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./AnalysisPageHeader.module.css";
+import styles from "./AnalysisPageheader.module.css";
 
 const AnalysisPageheader = () => {
   return (
