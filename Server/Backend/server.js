4faa3e96738216b478d623b78ec1b45c413498b1
ty2arjit <ty2arjit@gmail.com>
2025-08-03 11:38:55 +0530

    before_deploy

--- Server/Backend/server.js
+++ Server/Backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true
 }));
 app.use(express.json());
