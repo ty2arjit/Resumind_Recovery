fbc0c1bfd42fd0b5793a403b186189917cc1bcda
ty2arjit <ty2arjit@gmail.com>
2025-10-18 11:55:26 +0530

    Validation of authorisation

---
+++ Server/Backend/middleware/AuthValidation.js
@@ -0,0 +1,34 @@
+const Joi = require('joi');
+
+const signupValidation = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    college: Joi.string(),
+    email: Joi.email().required(),
+    password: Joi.string().required()
+  });
+
+  const {error} = schema.validate(req.body);
+  if(error) {
+    res.status(400).json({message: "Bad request", error});
+  }
+  next();
+};
+
+const loginValidation = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.email().required(),
+    password: Joi.string().required()
+  })
+
+  const { error } = schema.validate(req.body);
+  if(error) {
+    res.status(400).json({message: "Bad credentials", error});
+  }
+  next();
+}
+
+module.exports = {
+  signupValidation,
+  loginValidation
+}
